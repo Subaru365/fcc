89747a7333c5f3de2acba3e0d4f9d056155e1497
Akira Murata <akira-murata@dac.co.jp>
2018-11-11 02:14:06 +0900

    :sunny: テストコードを追加

---
+++ fcc/test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+try() {
+	expected="$1"
+	input="$2"
+
+	./fcc "$input" >tmp.s
+	gcc -o tmp tmp.s
+	./tmp
+	actual="$?"
+
+	if [ "$actual" != "$expected" ]; then
+		echo "$input expected, but got $actual"
+		exit 1
+	fi
+}
+
+try 0 0
+try 42 42
+
+echo OK
