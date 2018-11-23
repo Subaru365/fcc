d3c16febcc9f5f32c7b100737e320b718a792a23
Akira Murata <akira-murata@dac.co.jp>
2018-11-23 12:33:29 +0900

    :beer: step3:トークナイザを導入

--- fcc.c
+++ fcc.c
@@ -1,39 +1,114 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char const *argv[])
+// Token value
+enum
+{
+    TK_NUM = 256,
+    TK_EOF,
+};
+
+// Token type
+typedef struct
+{
+    int ty;
+    int val;
+    char *input;
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p)
+{
+    int i = 0;
+    while (*p)
+    {
+        // Skip spaces
+        if (isspace(*p))
+        {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-')
+        {
+            tokens[i].ty = *p;
+            tokens[i].input = p;
+            i++;
+            p++;
+            continue;
+        }
+
+        if (isdigit(*p))
+        {
+            tokens[i].ty = TK_NUM;
+            tokens[i].input = p;
+            tokens[i].val = strtol(p, &p, 10);
+            i++;
+            continue;
+        }
+
+        fprintf(stderr, "Can't tokenize: %s\n", p);
+        exit(1);
+    }
+
+    tokens[i].ty = TK_EOF;
+    tokens[i].input = p;
+}
+
+// Error handler
+void error(int i)
+{
+    fprintf(stderr, "Unexpected Token: %s\n", tokens[i].input);
+    exit(1);
+}
+
+int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        fprintf(stderr, "引数の個数が正しくありません\n");
+        fprintf(stderr, "Invalid args count\n");
         return 1;
     }
 
-    char *p = argv[1];
+    tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".global _main\n");
     printf("_main:\n");
-    printf("    mov rax, %ld\n", strtol(p, &p, 10));
 
-    while (*p)
+    // Ensure that initial value is number
+    if (tokens[0].ty != TK_NUM)
+        error(0);
+    printf("    mov rax, %d\n", tokens[0].val);
+
+    // Print assembly
+    int i = 1;
+    while (tokens[i].ty != TK_EOF)
     {
-        if (*p == '+')
+        if (tokens[i].ty == '+')
         {
-            p++;
-            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            i++;
+            if (tokens[i].ty != TK_NUM)
+                error(i);
+            printf("    add rax, %d\n", tokens[i].val);
+            i++;
             continue;
         }
 
-        if (*p == '-')
+        if (tokens[i].ty == '-')
         {
-            p++;
-            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            i++;
+            if (tokens[i].ty != TK_NUM)
+                error(i);
+            printf("    sub rax, %d\n", tokens[i].val);
+            i++;
             continue;
         }
 
-        fprintf(stderr, "予期せぬ文字です: '%c'\n", *p);
-        return 1;
+        error(i);
     }
 
     printf("    ret\n");
